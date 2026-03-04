05fafc62a23ffa685166fada0691b95c60b08d62
devashishmani <161852655+devashishmani@users.noreply.github.com>
2026-03-04 10:44:36 +0530

    Time: 3 ms (97.49%) | Memory: 115.9 MB (28.96%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -1,24 +1,17 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
         long result = 0;
-        int zeroCount = 0;
-        
-        for (int num : nums) {
-            // Check if the current number is zero
-            if (num == 0) {
-                // Extend the current zero sequence
-                zeroCount++;
-            } else {
-                // Calculate subarrays for the zero sequence
-                result += zeroCount * (zeroCount + 1L) / 2;
-                // Reset zero sequence counter
-                zeroCount = 0;
-            }
+         int zeroCount = 0;
+
+    for(int i =0;i<nums.length;i++){
+        if(nums[i] == 0){
+            zeroCount++;
+            result += zeroCount;
+        } else {
+            zeroCount = 0;
         }
+    }
+    return result;
         
-        // If the last element was part of a zero sequence, add its subarrays
-        result += zeroCount * (zeroCount + 1L) / 2;
-        
-        return result;
     }
 }
